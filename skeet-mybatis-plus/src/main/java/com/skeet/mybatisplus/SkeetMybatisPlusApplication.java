f58543db3ef81aaae5e03880a102b96b563a42d5
chengsijie <chengsj@qtrade.com.cn>
2021-01-27 17:25:58 +0800

    feat(mybatis-plus): 初始化mybatis-plus包

---
+++ skeet-mybatis-plus/src/main/java/com/skeet/mybatisplus/SkeetMybatisPlusApplication.java
@@ -0,0 +1,13 @@
+package com.skeet.mybatisplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkeetMybatisPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkeetMybatisPlusApplication.class, args);
+	}
+
+}
